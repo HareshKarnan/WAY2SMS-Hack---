ec399ef9dc2128e1da4fa01c05569d014f67f39e
Haresh Miriyala <haresh.miriyala@gmail.com>
2014-06-18 20:57:42 +0530

    Update hackintoway2sms.py

--- hackintoway2sms.py
+++ hackintoway2sms.py
@@ -1,11 +1,14 @@
 from splinter import Browser
 browser=Browser('firefox')
 browser.visit('http://www.way2sms.com/content/index.html')
-browser.find_by_name('username').fill('mobno')
-browser.find_by_name('password').fill('pas')
+browser.find_by_name('username').fill('mob')
+browser.find_by_name('password').fill('pass')
 browser.find_by_name('Login').click()
 browser.find_by_css('.boo.I4').click()
 browser.find_by_id('quicksms').click()
-browser.find_by_name('textArea').click()
-browser.find_by_css('.frm-m .inp').click()
-browser.find_by_name('Send').click()
+
+with browser.get_iframe('frame') as iframe:  
+    browser.find_by_name('textArea').fill('textttareaarereara')
+    browser.find_by_css('.inp').fill('9710101067')
+    browser.find_by_name('Send').click()
+
